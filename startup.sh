f5b2c3b557d070e4ce6ce88b4fbe38b8ebbe27bc
EllenneedBuff075 <poo1600hn@gmail.com>
2025-11-08 22:17:39 -0600

    Cambios
    Para probar

--- startup.sh
+++ startup.sh
@@ -2,8 +2,21 @@ echo "Instalando Java, por favor espera..."
 sudo apt-get update > /dev/null 2>&1
 sudo apt-get install -y openjdk-17-jre-headless > /dev/null 2>&1
 echo "Java instalado."
+echo ""
 
-# Inicia el servidor de Minecraft
+if [ ! -f "server.properties" ]; then
+    echo "Detectado primer inicio del servidor. Realizando configuración inicial..."
+    echo "eula=true" > eula.txt
+    echo "level-name=${WORLD_NAME:-world}" > server.properties
+    
+    if [ -n "$WORLD_SEED" ]; then
+        echo "level-seed=${WORLD_SEED}" >> server.properties
+        echo "Usando semilla personalizada: $WORLD_SEED"
+    else
+        echo "No se especificó semilla, se usará una aleatoria."
+    fi
+    echo ""
+fi
+
 echo "Iniciando servidor de Minecraft..."
-# ASEGÚRATE DE QUE EL NOMBRE DEL .JAR COINCIDE CON EL QUE SUBISTE
-java -Xms4G -Xmx6G -jar paper-1.21.8-60.jar nogui 
+java -Xms1G -Xmx3G -jar paper-1.21.8-60.jar nogui
